b3e73c5038344a1787afac495297f85f5880bde6
David Garrood <dgarrood@hotmail.com>
2018-03-15 12:04:51 +0100

    Laid out comment 'blueprint' for how script should be made - variables etc

--- js/app.js
+++ js/app.js
@@ -1,1 +1,58 @@
 // JavaScript file
+
+//Variables needed
+//Card symbols IE dog, cat, bird 'all start at 0'
+//Click counter
+//Timer
+//Game won boolean
+//?
+
+//Create cards
+//New div - add class
+//New h1 - add class
+//Randomise h1 content out of 8 possible symbols
+//Check counter for symbol chosen
+//If === 2
+//Reroll symbol
+//If ==< 2
+//Add count to symbol chosen IE id=dog, dogCounter +=
+//Add ID to h1 based on symbol
+//object div - append h1
+//Group 4 card objects as 1 object
+//Run script 16 times
+
+//Append Cards to document body
+//Hide rows
+//Select row
+//Append group of 4 cards element
+//Select new row, repeat
+//Unhide rows
+
+//Click script
+//CSS Change on click IE card highlighted and flipped
+//Variable for current selected card
+//Variable for second selected card
+//Add +1 to click counter
+//Check if card IDs match
+//If Yes
+//Active boolean to keep cards flipped
+//Add + 1 to 'pairs variable'
+//Check 'pairs variable === 8'
+  //If Yes
+  //Set game won to yes
+  //Set page to 'you won' page
+  //If No
+  //Do nothing
+//If No
+//Activate script to flip back over
+
+//New game button
+//Hide all rows
+//Delete children of all rows
+//Run 'Create Cards' script
+//Run 'Append Cards' script
+//Unhide rows
+
+//Timer button
+//On click start counter
+//When game is 'won' counter stops
